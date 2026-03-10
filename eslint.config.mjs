939e167df11fcf466c43d654a27a2c24fad5ebe7
nesti-stark <nesti.s.dev@outlook.com>
2026-03-10 18:52:15 -0400

    Enhance ESLint configuration with additional plugins and rules for improved code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
+import PrettierConfig from 'eslint-config-prettier';
+import ImportPlugin from 'eslint-plugin-import';
+import ReactPlugin from 'eslint-plugin-react';
+import ReactHooksPlugin from 'eslint-plugin-react-hooks';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +12,61 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
+    'node_modules/**',
     '.next/**',
     'out/**',
     'build/**',
     'next-env.d.ts',
   ]),
+  {
+    plugins: {
+      import: ImportPlugin,
+      react: ReactPlugin,
+      reactHooks: ReactHooksPlugin,
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: true,
+          fixStyle: 'separate-type-imports',
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          pathGroups: [
+            { pattern: 'react', group: 'external', position: 'before' },
+            { pattern: 'react-*', group: 'external', position: 'before' },
+            { pattern: 'react-*/**', group: 'external', position: 'before' },
+            { pattern: 'next', group: 'external', position: 'before' },
+            { pattern: 'next/**', group: 'external', position: 'before' },
+            { pattern: 'next-*', group: 'external', position: 'before' },
+            { pattern: 'next-*/**', group: 'external', position: 'before' },
+            { pattern: '@/**', group: 'internal', position: 'after' },
+            { pattern: '~/**', group: 'internal', position: 'after' },
+          ],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'never',
+          warnOnUnassignedImports: true,
+        },
+      ],
+      'sort-imports': [
+        'error',
+        {
+          ignoreDeclarationSort: true,
+          ignoreCase: true,
+        },
+      ],
+      'react/jsx-curly-brace-presence': [
+        'error',
+        { props: 'always', children: 'ignore', propElementValues: 'always' },
+      ],
+    },
+  },
+  PrettierConfig,
 ]);
 
 export default eslintConfig;
